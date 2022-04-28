cb28f872c6d8ed98fc08d698251460f4918523f1
Benedikt Keil <benedikt.keil@makonis.de>
2022-04-28 11:02:19 +0200

    Parallel Sessions without Tracks

--- src/app/agenda/components/day/day.component.ts
+++ src/app/agenda/components/day/day.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { SessionResource } from 'src/app/model/session.resource';
 import { SessionService } from 'src/app/services/session.service';
+import { SettingsService } from 'src/app/services/settings.service';
 
 @Component({
   selector: 'app-day',
@@ -12,7 +13,7 @@ import { SessionService } from 'src/app/services/session.service';
 export class DayComponent implements OnInit {
 
   get hasTracks(): boolean {
-    return this.tracks.length > 1;
+    return this.tracks.length > 1 && this._settings.settings.tracksActivated;
   }
 
   get tracks(): string[] {
@@ -24,7 +25,11 @@ export class DayComponent implements OnInit {
   }
 
   get sessions(): SessionResource[] {
-    return this._session.getSessionsByDay(this._day!);
+    return this._session.getSessionsByDayandFilteredWithParallelSessions(this._day!) ?? [];
+  }
+
+  get parallelSessions(): SessionResource[] {
+    return this._session.getParallelSessions(this._session.getSessionsByDay(this._day!)!) ?? [];
   }
 
   get isMobile(): boolean {
@@ -36,7 +41,8 @@ export class DayComponent implements OnInit {
   constructor(
     private _route: ActivatedRoute,
     private _session: SessionService,
-    private _device: DeviceDetectorService
+    private _device: DeviceDetectorService,
+    private _settings: SettingsService
   ) { }
 
   ngOnInit(): void {
