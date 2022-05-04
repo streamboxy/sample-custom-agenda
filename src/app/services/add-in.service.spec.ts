c9fe03fbaa79603314c939d2a54aeea19f849a9f
Benedikt Keil <benedikt.keil@makonis.de>
2022-05-04 13:52:09 +0200

    Fix Tests

--- src/app/services/add-in.service.spec.ts
+++ src/app/services/add-in.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AddInService } from './add-in.service';
+import { LanguageService } from './language.service';
 
 describe('AddInService', () => {
   let service: AddInService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {provide: LanguageService, useValue: jasmine.createSpyObj('LanguageService', ['switchLanguage'] )}
+      ]
+    });
     service = TestBed.inject(AddInService);
   });
 
